36b5c6c1b96cca5df8206fb8f1e4562b0d13f22b
Jordy Wira Arta <bion.internship09@binus.edu>
2025-03-18 18:21:08 +0700

    Try push again

--- src/pages/Manage.tsx
+++ src/pages/Manage.tsx
@@ -1,7 +1,15 @@
 import "../index.css";
 
 const Manage = () => {
-  return <div>Manage Page</div>;
+  return (
+    <div className="flex flex-col items-center justify-items-center p-10 h-full">
+      <div className="flex flex-row space-x-4 p-4 bg-gray-800 rounded-lg w-fit">
+        <div className="text-6xl font-bold text-white w-fit text-center">
+          {String(1).padStart(3, "0")}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Manage;
